6aefbd34fdbd937866ac467db2451636701a7a1e
james lockwood <ksjjl05@gmail.com>
2021-09-08 18:58:37 -0500

    cleanup on the3 plugins layout

--- HBB/inc/Base/Enqueue.php
+++ HBB/inc/Base/Enqueue.php
@@ -5,8 +5,9 @@
 
 
  namespace Inc\Base;
- 
- class Enqueue
+ use \Inc\Base\BaseController;
+
+ class Enqueue extends BaseController
  {
 
     public function register(){
@@ -15,8 +16,8 @@
     }
     function enqueue() {
         // enqueue all our scripts
-        wp_enqueue_style( 'mypluginstyle', PLUGIN_URL .  './assets/mystyle.css'  );
-        wp_enqueue_script( 'mypluginscript', PLUGIN_URL .  './assets/myscript.js'  );
+        wp_enqueue_style( 'mypluginstyle', $this->plugin_url .  './assets/mystyle.css'  );
+        wp_enqueue_script( 'mypluginscript', $this->plugin_url .  './assets/myscript.js'  );
     }
  }
  

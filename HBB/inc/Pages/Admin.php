5aa568a2cdce90e855af895993e18446ef865b67
james lockwood <ksjjl05@gmail.com>
2021-09-10 16:55:30 -0500

    adding new settings API setup to the plugin

--- HBB/inc/Pages/Admin.php
+++ HBB/inc/Pages/Admin.php
@@ -4,20 +4,37 @@
  */
 namespace Inc\Pages;
 
+use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
+
 /**
 * 
 */
-class Admin
+class Admin extends BaseController
 {
-	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+	public $settings;
+
+	public $pages = array();
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'HBB Plugin', 
+				'menu_title' => 'HBB', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'hbb_plugin', 
+				'callback' => function() { echo '<h1>HBB Plugin</h1>'; }, 
+				'icon_url' => 'dashicons-store', 
+				'position' => 110
+			)
+		);
 	}
 
-	public function add_admin_pages() {
-		add_menu_page( 'HBB Plugin', 'HBB', 'manage_options', 'hbb_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-	}
-
-	public function admin_index() {
-		require_once PLUGIN_PATH . 'templates/admin.php';
+	public function register() 
+	{
+		$this->settings->addPages( $this->pages )->register();
 	}
 }

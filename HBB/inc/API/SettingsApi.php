1759fb6c9448104e7ce62e40807e12a917720fab
james lockwood <ksjjl05@gmail.com>
2021-09-10 17:50:31 -0500

    adding sub pages to the dashboard

--- HBB/inc/API/SettingsApi.php
+++ HBB/inc/API/SettingsApi.php
@@ -9,6 +9,8 @@ class SettingsApi
 {
 	public $admin_pages = array();
 
+    public $admin_subpages;
+
 	public function register()
 	{
 		if ( ! empty($this->admin_pages) ) {
@@ -22,11 +24,42 @@ class SettingsApi
 
 		return $this;
 	}
+    public function withSubPage( string $title = null ){
+        if ( empty($this->admin_pages)){
+            return $this;
+        }
+        $admin_page = $this->admin_pages[0];
+
+
+        $subPages =  array(
+			array(
+                'parent_slug' => $admin_page['menu_slug'],
+				'page_title' => $admin_page['page_title'],
+				'menu_title' => ($title) ? $title : $admin_page['menu_title'],
+				'capability' => $admin_page['capability'],
+				'menu_slug' => $admin_page['menu_slug'],
+				'callback' => $admin_page['callback'], 
+				
+			)
+		);
+        $this->admin_subpages = $subPages;
+
+        return $this;
+    }
+    public function addSubPages( array $pages ){
+
+        $this->admin_subpages = array_merge($this->admin_subpages, $pages);
+        
+        return $this;
+    }
 
 	public function addAdminMenu()
 	{
 		foreach ( $this->admin_pages as $page ) {
 			add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position'] );
+		}
+        foreach ( $this->admin_subpages as $page ) {
+			add_submenu_page( $page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'] );
 		}
 	}
 }
--- HBB/inc/Pages/Admin.php
+++ HBB/inc/Pages/Admin.php
@@ -15,6 +15,7 @@ class Admin extends BaseController
 	public $settings;
 
 	public $pages = array();
+	public $subpages = array();
 
 	public function __construct()
 	{
@@ -31,10 +32,39 @@ class Admin extends BaseController
 				'position' => 110
 			)
 		);
+		$this->subpages =  array(
+			array(
+                'parent_slug' => 'hbb_plugin',
+				'page_title' => 'Custom Post Types',
+				'menu_title' => 'CPT',
+				'capability' => 'manage_options',
+				'menu_slug' => 'hbb_cpt',
+				'callback' => function() { echo '<h1>Custom Post Types Manger</h1>'; }, 
+				
+			),
+			array(
+                'parent_slug' => 'hbb_plugin',
+				'page_title' => 'Custom Taxonomies',
+				'menu_title' => 'Taxonomies',
+				'capability' => 'manage_options',
+				'menu_slug' => 'hbb_taxonomies',
+				'callback' => function() { echo '<h1>Taxonomies Manger</h1>'; }, 
+				
+			),
+			array(
+                'parent_slug' => 'hbb_plugin',
+				'page_title' => 'Custom Widgets',
+				'menu_title' => 'Widgets',
+				'capability' => 'manage_options',
+				'menu_slug' => 'hbb_widgets',
+				'callback' => function() { echo '<h1>Widgets Manger</h1>'; }, 
+				
+			)
+		);
 	}
 
 	public function register() 
 	{
-		$this->settings->addPages( $this->pages )->register();
+		$this->settings->addPages( $this->pages )->withSubPage('Dashboard')->addSubPages( $this->subpages)->register();
 	}
 }
